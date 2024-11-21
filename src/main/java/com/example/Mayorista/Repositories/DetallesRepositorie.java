685355442f13fef58ae6108f013dda132fcd7f58
JulioMonty <juliocmontalvo74@gmail.com>
2024-11-21 10:44:15 -0600

    Delete src directory

--- src/main/java/com/example/Mayorista/Repositories/DetallesRepositorie.java
+++
@@ -1,9 +0,0 @@
-package com.example.Mayorista.Repositories;
-
-import com.example.Mayorista.Models.DetallesModelo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DetallesRepositorie extends CrudRepository<DetallesModelo,String> {
-}
