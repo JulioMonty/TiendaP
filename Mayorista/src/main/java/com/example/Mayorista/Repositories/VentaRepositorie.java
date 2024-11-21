5f21411a583d9f48d0c597d8220596d6c0fbd7cc
JulioMonty <juliocmontalvo74@gmail.com>
2024-11-21 11:22:06 -0600

    Delete Mayorista directory

--- Mayorista/src/main/java/com/example/Mayorista/Repositories/VentaRepositorie.java
+++
@@ -1,9 +0,0 @@
-package com.example.Mayorista.Repositories;
-
-import com.example.Mayorista.Models.VentaModelo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VentaRepositorie extends CrudRepository<VentaModelo,String> {
-}
