896d7cddd61ec8776f22ac0e10e4ed9a23ae6402
JulioMonty <juliocmontalvo74@gmail.com>
2024-11-23 16:52:25 -0600

    Add files via upload

---
+++ Mayorista/src/main/java/com/example/Mayorista/Configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.Mayorista.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://tiendap-production-86e7.up.railway.app/api/solicitudes/crearPunto") // Agrega aquí el dominio permitido
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // Si estás manejando cookies o sesiones
+    }
+}
