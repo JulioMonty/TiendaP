c123aab725ccb7f07256610c0367c6647aa19d09
JulioMonty <juliocmontalvo74@gmail.com>
2024-11-24 23:09:19 -0600

    Delete SolicitudRepo.java

--- SolicitudRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Mayorista.Repositories;
-
-import com.example.Mayorista.Models.SolicitudesModelo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SolicitudRepo extends CrudRepository<SolicitudesModelo,String> {
-}
