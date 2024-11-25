6fd899ae4d2b71c9a239b6cbd18c9a2719443eb4
JulioMonty <juliocmontalvo74@gmail.com>
2024-11-24 23:08:34 -0600

    Delete Mayorista directory

--- Mayorista/src/main/java/com/example/Mayorista/Repositories/ConfigRepositorie.java
+++
@@ -1,9 +0,0 @@
-package com.example.Mayorista.Repositories;
-import com.example.Mayorista.Models.ConfigModelo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfigRepositorie extends CrudRepository<ConfigModelo,String>{
-
-}
